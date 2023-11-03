6f7b2031b0ef4b407782b25b71404d426ada8aca
Amir <theamirm@gmail.com>
2023-11-04 02:12:24 +0330

    feat: verification emails now working

--- src/middleware.ts
+++ src/middleware.ts
@@ -17,9 +17,12 @@ import { withAuth } from "next-auth/middleware"
 export default withAuth({
   // Matches the pages config in `[...nextauth]`
   pages: {
-    signIn: '/login',
-    error: '/error',
-  }
+    signIn: '/auth/login',
+    // signOut: '/auth/logout',
+    error: '/auth/error', // Error code passed in query string as ?error=
+    verifyRequest: '/auth/verify-request', // (used for check email message)
+    // newUser: '/auth/new-user', // New users will be directed here on first sign in (leave the property out if not of interest)
+  },
 })
 
 export async function middleware(req: NextRequest) {
